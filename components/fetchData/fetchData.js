a498b40211455564d2fb4158b49f6866aafe4cf4
Joachim Lehmann <joa.lehmann@web.de>
2022-07-15 11:26:37 +0200

    Connected API

--- components/fetchData/fetchData.js
+++ components/fetchData/fetchData.js
@@ -1,3 +1,8 @@
 export default function fetchData() {
-  console.log('HELLO');
+  const url = 'https://www.anapioficeandfire.com/api/houses';
+
+  fetch(url)
+    .then(jsonObject => jsonObject.json())
+    .then(javaScriptObject => displayHouses(javaScriptObject))
+    .catch(error => console.error(error));
 }
