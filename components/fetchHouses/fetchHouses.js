ec7bae4cbb6272ce2177955a0889901ad4d0eb4f
Andreas Kiener <andreas.kiener@gmail.com>
2022-07-15 17:50:57 +0200

    Refactor fetchData as async function

--- components/fetchHouses/fetchHouses.js
+++ components/fetchHouses/fetchHouses.js
@@ -1,9 +1,16 @@
 import displayHouses from '../mapHouses/mapHouses.js';
-export default function fetchData() {
+/*export default function fetchData() {
   const url = 'https://www.anapioficeandfire.com/api/houses';
 
   fetch(url)
     .then(jsonObject => jsonObject.json())
     .then(javaScriptObject => displayHouses(javaScriptObject))
     .catch(error => console.error(error));
+}*/
+
+export default async function fetchData() {
+  const url = 'https://www.anapioficeandfire.com/api/houses';
+  const data = await (await fetch(url)).json();
+  displayHouses(data);
+  console.log(data);
 }
