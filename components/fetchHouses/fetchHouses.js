0a92f8a145590c74f713aa43f89c53be4744e799
Andreas Kiener <andreas.kiener@gmail.com>
2022-07-19 09:46:51 +0200

    Fix husky

--- components/fetchHouses/fetchHouses.js
+++ components/fetchHouses/fetchHouses.js
@@ -21,21 +21,22 @@ import showHouses from '../showHouses/showHouses.js';
     .catch(error => console.error(error));
 } */
 
-/* This does basicly the same as the function above and has exactly the same problem: */
+/* The following does basicly the same as the function above and has exactly the same problem: */
 
 export default async function fetchData() {
   const url = 'https://www.anapioficeandfire.com/api/houses';
   const response = await fetch(url);
   const data = await response.json();
-  const result = await Promise.all(data.map(async house => {
-    if (house.currentLord) {
-      const currentLordResult = await fetch(house.currentLord); //fetch the data for the currtent lord
-      const currentLordData = await currentLordResult.json();
-      house.currentLordName = currentLordData.name; //set the name of the person fetch as currentLordName in houses data
-  
-    }
-    return house;
-  }));
+  const result = await Promise.all(
+    data.map(async house => {
+      if (house.currentLord) {
+        const currentLordResult = await fetch(house.currentLord); //fetch the data for the currtent lord
+        const currentLordData = await currentLordResult.json();
+        house.currentLordName = currentLordData.name; //set the name of the person fetch as currentLordName in houses data
+      }
+      return house;
+    })
+  );
 
   showHouses(result);
 }
